05fc8405b4ebddcc9db022da14df1b2f55f2e1e0
AmaliaMirisan <126557543+AmaliaMirisan@users.noreply.github.com>
2025-06-03 00:09:25 +0300

    Add files via upload

---
+++ GcccBackend/src/main/java/com/example/project/domain/dto/CarDTO.java
@@ -0,0 +1,67 @@
+package com.example.project.domain.dto;
+
+
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CarDTO {
+    private Long id;
+    private String model;
+    private String manufacturer;
+    private Integer productionYear;
+    private String imageUrl;
+    private MultipartFile image;
+
+    public MultipartFile getImage() {
+        return image;
+    }
+
+    public void setImage(MultipartFile image) {
+        this.image = image;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Integer getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(Integer productionYear) {
+        this.productionYear = productionYear;
+    }
+}
